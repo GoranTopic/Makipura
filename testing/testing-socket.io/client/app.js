b52198e9c2473a463859f91f9df021dfb93c89fc
telix <telix@telegonicaxx@live.com>
2021-10-15 14:03:11 -0700

    started client side chat application

--- testing/testing-socket.io/client/app.js
+++ testing/testing-socket.io/client/app.js
@@ -1,8 +1,47 @@
 import { io } from "socket.io-client";
+import fs from "fs";
 
-const socket = io("http://localhost:3000");
+const socket = io("http://localhost:3000", {
+		//rejectUnauthorized: false,
+		//cert: fs.readFileSync("../cert.pem"),
+		//secure: true,
+		autoConnect: false,
+});
 
-socket.on("connect", () => {  console.log(socket.id); });
-socket.on("disconnect", () => {  console.log(socket.id); });
+socket.on("connect", () => {  
+		console.log(socket.id); 
+		console.log("connected to server"); 
+});
 
-socket.on("connect_error", () => {  setTimeout(() => {    socket.connect();  }, 1000);});
+
+let other_users = new Set();
+
+socket.on("users", users => { 
+		// catch all event handling
+		users.forEach( user => {
+				if(user.userID !== socket.id) other_users.add(user.userID)
+		}  )
+		console.log(other_users);
+});
+
+socket.emit("get users");
+
+socket.on("connect_error", (err) => {
+  if (err.message === "could not connect to server") {
+    this.usernameAlreadySelected = false;
+  }
+});
+
+let message = "is anybody out there";
+
+socket.emit("private message", { 
+		content:  message,
+		to: other_users[0],
+})
+
+
+socket.auth = { username: "telix", };
+
+//socket.onAny((event, ...args) => console.log(event, args) );
+
+socket.connect() // manual connect
--- testing/testing-socket.io/server/server.js
+++ testing/testing-socket.io/server/server.js
@@ -1,29 +1,58 @@
-/*
+import express from 'express';
+import { createServer } from 'http';
 import { Server } from "socket.io";
-const io = new Server();
-io.on("connection", socket => { 
-		console.log("server got connection")
-		console.log(socket);
-});
+import fs from 'fs';
 
-io.listen(3000, ()=> { console.log("listening on wss://localhost:3000") });
-*/
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
-const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+
+const credentials = {
+		key: fs.readFileSync('../key.pem'),
+		cert: fs.readFileSync('../cert.pem'),
+		ca: fs.readFileSync('../csr.pem'),
+		requestCert: false,
+		rejectUnauthorized: false
+};
+
+
+// create https server
+const server = createServer(credentials, app);
+
+const io = new Server(server, { cors: 
+		{
+				origin: "http://localhost:8080", 
+		}, 
+});
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile('/home/telix/Makipura/testing/testing-socket.io/server/index.html');
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+
+io.on("connection", (socket) => {
+  console.log('new user connected');
+  const users = [];
+  for (let [id, socket] of io.of("/").sockets) {
+    users.push({
+      userID: id,
+      username: socket.username,
+    });
+  }
+  socket.emit("users", users);
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+io.on("private message", ({ content, to }) => {
+		socket.to(to).emit("private message", { 
+				content: content,
+				from: socket.id,  
+		})
+})
+
+io.on("get users", (input) =>{  
+		console.log(" get user server ran");
+		console.log(input);
+	
+})
+
+server.listen(3000, () => console.log('listening on http://localhost:3000'));
