bd17d00378a5a94098a7da633c1d635b489682b9
telix <telix@telegonicaxx@live.com>
2021-10-31 19:05:37 -0700

    took me the whole day to do it, but i finnaly got dot-env to work with react-native

--- frontend/App.js
+++ frontend/App.js
@@ -4,12 +4,11 @@ import { Provider } from 'react-redux'; // import reducer
 import { store, persistor } from './state/store.js'; 
 // call inizilizer to produce a state
 import { PersistGate } from 'redux-persist/integration/react';
-import dotenv from "dotenv";
+import { BASE_API  } from '@env';
 
 export default function App() {
-		dotenv.config()
 		console.log('BASE_API:');
-		console.log(process.env.BASE_API);
+		console.log(BASE_API);
 
 		return <>
 				<Provider store={store}>
